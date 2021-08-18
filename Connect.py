f4ef6a988954fee32334478f70c225fd62c2e62e
TariqT777 <TariqThomas777@gmail.com>
2021-08-18 16:07:19 -0400

    Can now read messages from a twitch streamer's chat in my console in realTime.

--- Connect.py
+++ Connect.py
@@ -1,20 +1,48 @@
+import socket
+from types import resolve_bases
 server = 'irc.chat.twitch.tv'
 port = 6667
-nickname = 'twitchChatReader'
+nickname = 'doctor_remarkable'
 token = 'oauth:qai92v51z01253epp7cpacy833uljy'
-channel = '#Otzdarva'
+channel = '#otzdarva'
 
 
 
-import socket
+
 
 sock = socket.socket()
 sock.connect((server,port))
 
 sock.send(f"PASS {token}\n".encode('utf-8'))
-sock.send(f"Nick {nickname}\n".encode('utf-8'))
+sock.send(f"NICK {nickname}\n".encode('utf-8'))
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
 
-resp = sock.recv(2048).decode('utf-8')
-print(resp)
+#resp = sock.recv(4096).decode('utf-8')
+#resp = sock.read(4096).decode('utf-8')
+#print(resp)
+
+
+def joinchat():
+    Loading = True
+    while Loading:
+        readbuffer_join = sock.recv(1024)
+        readbuffer_join = readbuffer_join.decode()
+        for line in readbuffer_join.split("\n"):
+            print(line)
+
+joinchat()
+
+'''
+        for line in readbuffer_join.split("/n"):
+            print(line)
+            Loading = loadingComplete(line)
+
+def loadingComplete(line):
+    if ("End of /NAMES list" in line):
+        return False
+    else:
+        return True
+'''
+
+
