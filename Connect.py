c01a97ce29cbb44830b501be720742eeef7bc4f7
TariqT777 <TariqThomas777@gmail.com>
2021-09-18 17:02:43 -0400

    Updates to the nav bar and creation of 404 and 500 pages

--- Connect.py
+++ Connect.py
@@ -38,6 +38,14 @@ def Index():
 '''    
     return render_template('index.html', data = printedDictionary)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
